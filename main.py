2b24f7b7854b703ca4bc469e04e52569dddd5da7
KirillMerz <kiri11.mer31yak0v@gmail.com>
2022-10-01 01:09:31 +0700

    Add docstrings, comments and type hintings

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""The script removes EXIF-data from images in the specified directory."""
+
 from pathlib import Path
 from datetime import datetime
 from PIL import Image
@@ -7,23 +9,30 @@ from PIL import Image
 IMAGE_FILES_PATTERN = '**/*.[jJiIbBpP][mMnNcCpP][oOeEgGpP]'
 
 
-def get_dir_size(dir_path):
+def get_dir_size(dir_path) -> int:
+    """Returns size of the directory in bytes."""
     size = 0
     for file_path in Path(dir_path).glob(IMAGE_FILES_PATTERN):
         size += Path(file_path).stat().st_size
     return size
 
 
-def process_image(image_path):
+def process_image(image_path) -> None:
+    """Removes EXIF-data from the image."""
     try:
         image = Image.open(image_path)
+
+        # Save image without EXIF-info
         image.save(image_path)
+
         print(f'[+] "{image_path}"')
     except Exception as exception:
         print(f'[-] "{image_path}": {exception}')
 
 
-def main():
+def main() -> None:
+    """Recursive scanning of the directory with images and reducing their size"""
+
     dir_path = input('Path to directory with photos: ')
     start_time = datetime.now()
     size_before = get_dir_size(dir_path)
